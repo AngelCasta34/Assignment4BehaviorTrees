ddff154f9b37188b043ebedcde6c45d1a6335071
aumai-ucsc <aaron.mai@gmail.com>
2025-05-08 21:24:17 -0700

    Wack Update Aaron
    Adjusted some values, but still not working as intented

--- AI/BehaviorBuilder.cs
+++ AI/BehaviorBuilder.cs
@@ -13,17 +13,20 @@ public class BehaviorBuilder
                 // Emergency heal if any ally below 50%
                 new Sequence(new BehaviorTree[] {
                     new AbilityReadyQuery("heal"),
+                    new NearbyEnemiesQuery(1, 5f),
                     new AnyAllyHealthLowQuery(0.5f),
                     new Heal()
                 }),
                 // Temporary strength buff
                 new Sequence(new BehaviorTree[] {
                     new AbilityReadyQuery("buff"),
+                    new NearbyEnemiesQuery(1, 5f),
                     new Buff()
                 }),
                 // Permanent strength buff
                 new Sequence(new BehaviorTree[] {
                     new AbilityReadyQuery("permaBuff"),
+                    new NearbyEnemiesQuery(1, 5f),
                     new PermaBuff()
                 }),
                 //  Attack if player in range
@@ -31,8 +34,18 @@ public class BehaviorBuilder
                     new IsPlayerInRangeQuery(agent.GetAction("attack").range),
                     new Attack()
                 }),
+                //Move and attack if there are more than 6 enemies in range
+                new Sequence(new BehaviorTree[] {
+                    new CountAlliesQuery(6, 10f),
+                    new MoveToPlayer(agent.GetAction("attack").range),
+                    new Attack()
+                }),
+                //Group at nearest waypoint if there are less than 9 allies in range
+                //new Sequence(new BehaviorTree[]{
+                //    new GoTo(AIWaypointManager.Instance.GetClosest())
+                //}),
                 // Fallback move to congregation point
-                new GoTo(AIWaypointManager.Instance.GetCongregationPoint(), agent.GetAction("attack").range)
+                new GoTowards(AIWaypointManager.Instance.GetCongregationPoint(), 10, agent.GetAction("attack").range)
             );
         }
         // front line meat shield
@@ -45,16 +58,14 @@ public class BehaviorBuilder
                     new MoveToPlayer(agent.GetAction("attack").range),
                     new Attack()
                 }),
-                //  Gather if fewer than 4 allies nearby
+                //  Attack if 6 or more allies nearby
                 new Sequence(new BehaviorTree[] {
-                    new CountAlliesQuery(4, 6f),
-                    new GoTo(AIWaypointManager.Instance.GetCongregationPoint(), agent.GetAction("attack").range)
-                }),
-                //  Default advance & attack
-                new Sequence(new BehaviorTree[] {
+                    new CountAlliesQuery(6, 10f),
                     new MoveToPlayer(agent.GetAction("attack").range),
                     new Attack()
-                })
+                }),
+                //  Congregate
+                new GoTowards(AIWaypointManager.Instance.GetCongregationPoint(), 10, agent.GetAction("attack").range)
             );
         }
         // hit and run striker
@@ -66,19 +77,19 @@ public class BehaviorBuilder
                     new IsPlayerInRangeQuery(agent.GetAction("attack").range),
                     new Attack()
                 }),
-                //  Retreat if health is low (<40%)
+                //  Strike when backed by at least 6 allies
                 new Sequence(new BehaviorTree[] {
-                    new IsHealthLowQuery(0.4f),
-                    new GoTo(AIWaypointManager.Instance.GetCongregationPoint(), agent.GetAction("attack").range)
-                }),
-                //  Strike when backed by at least 3 allies
-                new Sequence(new BehaviorTree[] {
-                    new CountAlliesQuery(3, 5f),
+                    new CountAlliesQuery(6, 10f),
                     new MoveToPlayer(agent.GetAction("attack").range),
                     new Attack()
                 }),
+                //  Retreat if health is low (<40%)
+                /*new Sequence(new BehaviorTree[] {
+                    new IsHealthLowQuery(0.4f),
+                    new GoTo(AIWaypointManager.Instance.GetCongregationPoint(), agent.GetAction("attack").range)
+                }),*/
                 //  Fallback 
-                new GoTo(AIWaypointManager.Instance.GetCongregationPoint(), agent.GetAction("attack").range)
+                new GoTowards(AIWaypointManager.Instance.GetCongregationPoint(), 10, agent.GetAction("attack").range)
             );
         }
         else
